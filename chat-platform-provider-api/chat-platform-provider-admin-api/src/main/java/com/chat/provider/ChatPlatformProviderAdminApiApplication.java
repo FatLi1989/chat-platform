3e2daa57b64b622d2aca7ef6974bf475e16aefcd
李彦鹏 <514340686@qq.com>
2019-07-22 17:53:36 +0800

    fix server

--- chat-platform-provider-api/chat-platform-provider-admin-api/src/main/java/com/chat/provider/ChatPlatformProviderAdminApiApplication.java
+++ chat-platform-provider-api/chat-platform-provider-admin-api/src/main/java/com/chat/provider/ChatPlatformProviderAdminApiApplication.java
@@ -2,7 +2,13 @@ package com.chat.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author Liyanpeng
+ * @date 2019/7/22 17:49
+ **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ChatPlatformProviderAdminApiApplication {
 
