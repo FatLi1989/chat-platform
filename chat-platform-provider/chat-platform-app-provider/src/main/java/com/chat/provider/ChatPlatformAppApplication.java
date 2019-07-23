b548d39863e21fc8dfceb4b325a5030aaa7e28df
李彦鹏 <514340686@qq.com>
2019-07-23 15:04:09 +0800

    practice

---
+++ chat-platform-provider/chat-platform-app-provider/src/main/java/com/chat/provider/ChatPlatformAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPlatformAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPlatformAppApplication.class, args);
+    }
+
+}
